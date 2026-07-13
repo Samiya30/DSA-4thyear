77dcaf0a60bde1380119c746c35fd765f43e2432
samiya <samiyasharma05@gmail.com>
2026-07-13 15:15:53 +0530

    done

---
+++ Lec-5/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args) {
+        String s="samiya";
+        String s1="sonam";
+        String s2="samiya";
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+        System.out.println(s.charAt(3));
+        System.out.println(s1.equals(s));
+        System.out.println(s.indexOf('m'));
+        System.out.println(s.equals(s2));
+        System.out.println(s==s2);
+    }
+}
